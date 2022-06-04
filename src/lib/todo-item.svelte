4803b5775b1e800f927aee050bef05448f509fb4
Fedor Skrebnev <f.skrebnev@eurodk.com>
2022-06-04 16:08:54 +0200

    add layout

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,17 @@
+<div>
+
+    <form action="" method="">
+        <input type="hidden" name="done" value=""/>
+        <button aria-label="Mark Done/Not Done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save ToDo">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete ToDo">Delete</button>
+    </form>
+    
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TodoItem from "../lib/todo-item.svelte";
+    let head = "Todo's";
+</script>
+
+<svelte:head>
+    <title>{head}</title>
+</svelte:head>
+
+<h1>{head}</h1>
+
+<form action="" method="">
+    <input type="text" name="text" aria-label="Add a todo" placeholder="+ add to todo" />
+</form>
+
+<TodoItem />
